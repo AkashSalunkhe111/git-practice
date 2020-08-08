a9a889825c6e485f80718e319fd56ab69c49ccd1
Akash Sallunkhe <salunkheakash111@gmail.com>
2020-08-09 02:03:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const name = "Akash"
+
+let message = "This is git practice"
+
+
+function printMessage(name, message) {
+    return `${message} for ${name}`
+}
+
+console.log(printMessage(name,message));
